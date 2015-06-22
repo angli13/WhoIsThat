7fc6ee01aa194332779d9e037fe919d7ff3801da
Fernando <angli.fernando@gmail.com>
2015-06-21 23:09:24 -0500

    Android Build

---
+++ android/android/project/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/home/fernando/Meteor/WhoIsThat/.meteor/local/cordova-build/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/home/fernando/Meteor/WhoIsThat/.meteor/local/cordova-build/platforms/android/ant-build/CordovaApp.ap_ \
+/home/fernando/Meteor/WhoIsThat/.meteor/local/cordova-build/platforms/android/ant-build/classes.dex \
+/home/fernando/Meteor/WhoIsThat/.meteor/local/cordova-build/platforms/android/CordovaLib/ant-build/classes.jar \

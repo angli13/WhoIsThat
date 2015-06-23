1e4e3b855abb6c11b5dd4732390f2fd7a090219a
angli13 <angli.fernando@gmail.com>
2015-06-23 11:36:08 -0500

    bugs 3

--- android/android/project/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/fernando/Meteor/WhoIsThat/.meteor/local/cordova-build/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/fernando/Meteor/WhoIsThat/.meteor/local/cordova-build/platforms/android/CordovaLib/AndroidManifest.xml \
--- android/android/project/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/fernando/Meteor/WhoIsThat/.meteor/local/cordova-build/platforms/android/ant-build/AndroidManifest.xml : \
-/home/fernando/Meteor/WhoIsThat/.meteor/local/cordova-build/platforms/android/AndroidManifest.xml \
